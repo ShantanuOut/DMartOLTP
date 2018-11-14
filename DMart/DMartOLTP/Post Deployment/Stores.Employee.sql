9237416db47f88d3893caea447973d70c076670e
Shantanu Narale <shantanunarale062@outlook.com>
2018-11-14 21:08:14 +0530

    Removing the ManagerId from Employee table.

--- DMart/DMartOLTP/Post Deployment/Stores.Employee.sql
+++ DMart/DMartOLTP/Post Deployment/Stores.Employee.sql
@@ -2,15 +2,15 @@
 --Object Type : Post Deployment Script
 --Date Created : 25th October 2018
 --Name : Shantanu Narale
---Date Modified :
+--Date Modified : 14th November 2018, Adding other employees and removing ManagerId in the insert.
 --Purpose : Post Deployment Script for entering information of Master Employee
 /**********************************************************************************************/
 IF NOT EXISTS (SELECT TOP 1 1 FROM Stores.Employee WHERE EmployeeId = 1)
 BEGIN
 	BEGIN TRAN insEmp
 	BEGIN TRY
-		INSERT INTO Stores.Employee
-		VALUES ('SHANN', 1, '/', NEWID(), 'Address Line 1', NULL, NULL, 1, 'A+', 1, 'Owner')
+		INSERT INTO Stores.Employee(EmployeeCode, OrgPath, EntityPhoneId, Address1, Address2, Address3, StoreId, BloodGroup, IsManager, Designation)
+		VALUES ('SHANN', '/', NEWID(), 'Address Line 1', NULL, NULL, 1, 'A+', 1, 'Owner')
 		INSERT INTO Stores.Phone
 		VALUES ('+919130089763', 1)
 		INSERT INTO Stores.EmployeePhone
--- DMart/DMartOLTP/Stores/Tables/Employee.sql
+++ DMart/DMartOLTP/Stores/Tables/Employee.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [Stores].[Employee]
 (
 	[EmployeeId]		INT IDENTITY(1,1), 
-    [EmployeeCode]		CHAR(5) NOT NULL, 
-    [ManagerId]			INT NOT NULL, 
+    [EmployeeCode]		CHAR(5) NOT NULL,     
     [OrgPath]			hierarchyid NOT NULL, 
+	[OrgLev]			AS [OrgPath].GetLevel(),
     [EntityPhoneId]		UNIQUEIDENTIFIER NOT NULL DEFAULT (NEWID()), 
     [Address1]			VARCHAR(100) NULL, 
     [Address2]			VARCHAR(100) NULL, 
@@ -13,10 +13,26 @@
     [IsManager]			BIT NOT NULL DEFAULT (0), 
     [Designation]		VARCHAR(10) NULL, 
     CONSTRAINT [PK_Employee_EmployeeId] PRIMARY KEY ([EmployeeId]), 
-    CONSTRAINT [UQ_Employee_EmployeeCode] UNIQUE ([EmployeeCode]), 
-    CONSTRAINT [FK_Employee_Employee] FOREIGN KEY ([ManagerId]) REFERENCES [Stores].[Employee]([EmployeeId]), 
+    CONSTRAINT [UQ_Employee_EmployeeCode] UNIQUE ([EmployeeCode]),     
     CONSTRAINT [UQ_Employee_Phone] UNIQUE ([EntityPhoneId]), 
     CONSTRAINT [FK_Employee_Store] FOREIGN KEY ([StoreId]) REFERENCES [Stores].[Store]([StoreId]), 
     CONSTRAINT [CK_Employee_BloodGroup] CHECK ([BloodGroup] IN ('A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-')), 
     CONSTRAINT [CK_Employee_Designation] CHECK ([Designation] IN ('Owner', 'Manager', 'Senior', 'Junior'))
+)
+
+GO
+
+CREATE INDEX [IX_Employee_OrgPath_DF] 
+ON [Stores].[Employee] 
+(
+	[OrgPath]
+)
+
+GO
+
+CREATE INDEX [IX_Employee_OrgLevOrgPath_BF] 
+ON [Stores].[Employee] 
+(
+	[OrgLev],
+	[OrgPath]
 )
